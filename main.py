dfe40af78173c91ad4b326ec885af00d30cba4a1
Miao <tymiao@gwmail.gwu.edu>
2019-09-12 14:20:04 -0400

    slideseg3

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-import slideseg
+import slideseg3
 import os
+import sys
 
 def main():
     """
     Runs SlideSeg with the parameters specified in Parameters.txt
     :return: image chips and masks
     """
-
+    print(sys.version)
     def str2bool(value):
         return value.lower() in ("true", "yes", "1")
 
-    params = slideseg.load_parameters('Parameters.txt')
+    params = slideseg3.load_parameters('Parameters.txt')
     print('running __main__ with parameters: {0}'.format(params))
 
     if not os.path.isdir(params["slide_path"]):
@@ -20,11 +21,11 @@ def main():
         params["xml_path"] = xpath
 
         print('loading {0}'.format(filename))
-        slideseg.run(params, filename)
+        slideseg3.run(params, filename)
 
     else:
         for filename in os.listdir(params["slide_path"]):
-            slideseg.run(params, filename)
+            slideseg3.run(params, filename)
 
 if __name__ == "__main__":
     main()
